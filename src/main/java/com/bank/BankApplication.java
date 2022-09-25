ab918018769799dda744578bc092861428291054
barthibanS <sbarthiban@gmail.com>
2022-09-26 03:43:02 +0530

    Update BankApplication.java

--- src/main/java/com/bank/BankApplication.java
+++ src/main/java/com/bank/BankApplication.java
@@ -1,48 +1,14 @@
 package com.bank;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.example.bank.controller")
-public class BankApplication { //implements CommandLineRunner {
+public class BankApplication {
 
-//	@Autowired
-//	private JdbcTemplate template;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
-
-//	@Override
-//	public void run(String... args) throws Exception {
-//		
-//	//	template.update("INSERT INTO ACCOUNT(account_type, account_number) VALUES('current', 0012250016006)");
-//	}
-
-
-//@Bean
-//public HttpSessionEventPublisher httpSessionEventPublisher() {
-//    return new HttpSessionEventPublisher();
-//}
-//
-//@Bean
-//public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//    http
-//        .sessionManagement(session -> session
-//            .maximumSessions(1)
-//            .maxSessionsPreventsLogin(true)
-//        );
-//    return http.build();
-//}
 }
 
 //https://mkyong.com/maven/maven-jacoco-code-coverage-example/
